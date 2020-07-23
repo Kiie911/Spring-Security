87edb92dadb14b189eb7a7f4d8ca48e7a474d067
Kiie911 <62591869+Kiie911@users.noreply.github.com>
2020-07-23 17:09:30 +0700

    ThucHanh

---
+++ src/main/java/com/hiep/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.hiep.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebSecurityConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
